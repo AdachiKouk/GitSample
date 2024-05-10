b7c032214c655c09fd97e6762c5d3dc145c51cf7
AdachiKouk <kd1364522@st.kobedenshi.ac.jp>
2024-05-10 11:46:36 +0900

    Initial Commit

---
+++ GitSample/GitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSampleApp.swift
+//  GitSample
+//
+//  Created by adachikouki on 2024/05/10.
+//
+
+import SwiftUI
+
+@main
+struct GitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
